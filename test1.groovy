4541b342987e9bfa18908041d80adaea2f10f977
arginbest <bsiitov84@gmail.com>
2021-01-06 23:51:40 +0000

    fromscript

--- test1.groovy
+++ test1.groovy
@@ -22,3 +22,18 @@ def notifyFailed() {
       
     )
 }
+
+
+        // stage('mail notification') {
+        //     emailext body: 'need attention', subject: 'jenkins job', to: 'baurzhansiit@gmail.com'
+        // }
+        // currentBuild.result = "FAILED"
+        // notifyFailed()
+
+        // mail bcc: '', 
+        // body: "Check console output at '${env.BUILD_URL}' error output = ${err}", 
+        // cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
+        //     to: 'baurzhansiit@gmail.com'
+        // emailext body: "${err}", subject: 'fail', to: 'baurzhansiit@gmail.com'
+
+
