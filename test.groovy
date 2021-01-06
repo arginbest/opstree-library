82c7f6a0991c0c4ff553a2553d8bacd3875c4df4
arginbest <bsiitov84@gmail.com>
2021-01-06 23:23:29 +0000

    fromscript

--- test.groovy
+++ test.groovy
@@ -6,32 +6,29 @@ node{
                 sh "echo hello"
                 sh "exit 1"
             }
-
+        currentBuild.result = "FAILED"
+        notifyFailed()
         }
         }, b: {
             timestamps {
             stage('two') {
                 sh "echo world"
             }
-            // stage('mail notification') {
-            //     emailext body: 'need attention', subject: 'jenkins job', to: 'baurzhansiit@gmail.com'
-            // }
+            stage('mail notification') {
+                emailext body: 'need attention', subject: 'jenkins job', to: 'baurzhansiit@gmail.com'
             }
+        }
         },
         failFast: true
     } 
     
-    catch (e) {
-        currentBuild.result = "FAILED"
-        notifyFailed()
+    catch (err) {
+        mail bcc: '', 
+        body: "Check console output at '${env.BUILD_URL}' error output = ${err}", 
+        cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
+            to: 'baurzhansiit@gmail.com'
+        // emailext body: "${err}", subject: 'fail', to: 'baurzhansiit@gmail.com'
     }
-}
-def notifyFailed() {
-    mail bcc: '', 
-    body: "Check console output at '${env.BUILD_URL}' error output = ${err}", 
-    cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
-        to: 'baurzhansiit@gmail.com'
-    // emailext body: "${err}", subject: 'fail', to: 'baurzhansiit@gmail.com'
 }
 
 
