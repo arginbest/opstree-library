28594df88296852626df4f179980629c43309684
arginbest <bsiitov84@gmail.com>
2021-01-06 23:30:43 +0000

    fromscript

---
+++ test2.groovy
@@ -0,0 +1,34 @@
+node{
+    
+        parallel a: {
+            timestamps {
+            stage('one') {
+                sh "echo hello"
+                sh "exit 1"
+            }
+        currentBuild.result = "FAILED"
+        notifyFailed()
+        }
+        }, b: {
+            timestamps {
+            stage('two') {
+                sh "echo world"
+            }
+            // stage('mail notification') {
+            //     emailext body: 'need attention', subject: 'jenkins job', to: 'baurzhansiit@gmail.com'
+            // }
+        }
+        },
+        failFast: true
+     
+    
+
+}
+
+        // mail bcc: '', 
+        // body: "Check console output at '${env.BUILD_URL}' error output = ${err}", 
+        // cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
+        //     to: 'baurzhansiit@gmail.com'
+        // emailext body: "${err}", subject: 'fail', to: 'baurzhansiit@gmail.com'
+
+
