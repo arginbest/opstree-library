a450c15552ebb761272278d28551e4e1e7996b75
arginbest <bsiitov84@gmail.com>
2021-01-06 22:34:36 +0000

    fromscript

--- test.groovy
+++ test.groovy
@@ -20,8 +20,7 @@ node{
         failFast: true
     } catch (err) {
         mail bcc: '', 
-        body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-        <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>hello from jenkins you have ${err}""", 
+        body: "Check console output at '${env.BUILD_URL}' error output = ${err}", 
         cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
             to: 'baurzhansiit@gmail.com'
         // emailext body: "${err}", subject: 'fail', to: 'baurzhansiit@gmail.com'
