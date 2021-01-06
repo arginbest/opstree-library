89ecdbe7396219f1f494d3e8780aa5c1a27bf6c0
arginbest <bsiitov84@gmail.com>
2021-01-06 22:06:24 +0000

    fromscript

--- test.groovy
+++ test.groovy
@@ -19,7 +19,7 @@ node{
         },
         failFast: true
     } catch (err) {
-        mail bcc: '', body: 'hello from jenkins', cc: '', from: '', replyTo: '', subject: 'jenkins-test', 
+        mail bcc: '', body: "hello from jenkins you have ${err}", cc: '', from: 'Jenkins', replyTo: '', subject: 'fail stage', 
             to: 'baurzhansiit@gmail.com'
         // emailext body: "${err}", subject: 'fail', to: 'baurzhansiit@gmail.com'
     }
