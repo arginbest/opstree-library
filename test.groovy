6abd45caf7f968acf09b7621473a872298998a84
arginbest <bsiitov84@gmail.com>
2021-01-06 22:16:53 +0000

    fromscript

--- test.groovy
+++ test.groovy
@@ -7,23 +7,21 @@ node{
                 sh "exit 1"
             }
         }
-            , b: {
-                    timestamps {
-                    stage('two') {
-                        sh "echo world"
-                    }
-                    stage('mail notification') {
-                        emailext body: 'need attention', subject: 'jenkins job', to: 'baurzhansiit@gmail.com'
-                        }
-                    }
-                }
-            },
-        failFast: true
-        } catch (err) {
-            mail bcc: '', body: "hello from jenkins you have ${err}", cc: '', from: '', replyTo: '', subject: 'jenkins-test', 
-                to: 'baurzhansiit@gmail.com'
-            // emailext body: "${err}", subject: 'fail', to: 'baurzhansiit@gmail.com'
+        }, b: {
+            timestamps {
+            stage('two') {
+                sh "echo world"
+            }
+            stage('mail notification') {
+                emailext body: 'need attention', subject: 'jenkins job', to: 'baurzhansiit@gmail.com'
+            }
         }
+        },
+        failFast: true
+    } catch (err) {
+        mail bcc: '', body: "hello from jenkins you have ${err}", cc: '', from: '', replyTo: '', subject: 'jenkins-test', 
+            to: 'baurzhansiit@gmail.com'
+        // emailext body: "${err}", subject: 'fail', to: 'baurzhansiit@gmail.com'
     }
 }
 
