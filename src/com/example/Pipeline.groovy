e7f7ed8f0475b8702722427062deffcaa2c2d1d9
arginbest <bsiitov84@gmail.com>
2021-01-07 10:07:43 +0000

    fromscript

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -16,7 +16,7 @@ class Pipeline {
  
        script.node() {
             script.stage('Read YAML file') {
-            script.commonPipelineEnvironment
+            script.this.script
             }
         
         }
