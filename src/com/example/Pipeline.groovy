bfa0b507dbca9c62d2987eb849f38e85b8b0edfe
arginbest <bsiitov84@gmail.com>
2021-01-07 07:26:13 +0000

    fromscript

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -16,7 +16,7 @@ class Pipeline {
     
     script.node() {
             script.stage('SCM') { 
-                script.sh  "checkout scm"
+                checkout scm
                 script.readYaml(file: "${WORKSPACE}/myconfig.yml") 
             }
     }
