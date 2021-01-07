c6687b85cb3e56aab76cc21f48065b6a8c1cd4d8
arginbest <bsiitov84@gmail.com>
2021-01-07 01:05:10 +0000

    fromscript

--- declarative.groovy
+++ declarative.groovy
@@ -9,10 +9,25 @@ pipeline {
     }
     post {
         always {
+        echo 'One way or another, I have finished'
+            deleteDir() /* clean up our workspace */
+        }
+        success {
+            echo 'I succeeded!'
+        }
+        unstable {
+            echo 'I am unstable :/'
+        }
+        failure {
+            echo 'I failed :('
         mail bcc: '', 
         body: "Check console output at '${env.BUILD_URL}'", 
         cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
         to: 'baurzhansiit@gmail.com'
+        }
+
+        changed {
+            echo 'Things were different before...'
         }
     }
 }
