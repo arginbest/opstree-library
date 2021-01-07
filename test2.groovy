9fd0130211010c1e4306758619f08fd18c5fe5de
arginbest <bsiitov84@gmail.com>
2021-01-07 00:18:29 +0000

    fromscript

--- test2.groovy
+++ test2.groovy
@@ -1,44 +1,21 @@
-pipeline {
-
-    agent any
-
-    stages {
-
-        stage('Build testJob') {
-
-            steps {
-                script {
-                    
-                    echo "Build of 'testJob' returned result:"
-
-                    
-                }
-            steps {
-                echo "second step"
-            }
+node{
+    try {
+            timestamps {
+            stage('one') {
+                sh "echo hello"
+                sh "exit 1"
             }
         }
-    }
-
-    post {
-
-        always {
-            echo "Build results"
-        }
-
-        success {
-            echo "All builds completed OK"
-        }
-
-        failure {
-            echo "A job failed"
-
-            script {
-                mail bcc: '', 
-                body: "Check console output at '${env.BUILD_URL}' error output = ${err}", 
-                cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
-                    to: 'baurzhansiit@gmail.com'
+            stage('two') {
+                sh "echo world"
             }
-        }
+        
+        }, failFast: true
+     
+    catch (err) {
+        mail bcc: '', 
+        body: "Check console output at '${env.BUILD_URL}' error output = ${err}", 
+        cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
+            to: 'baurzhansiit@gmail.com'
     }
 }
