00d9149494a01298d84e36e150a4fde6cf7a9af7
arginbest <bsiitov84@gmail.com>
2021-01-07 00:31:09 +0000

    fromscript

--- test2.groovy
+++ test2.groovy
@@ -1,6 +1,18 @@
 node {
     try {
-        sh 'might fail'
+        stage('one') {
+            sh "echo Hello; exit 1"
+        }
+        stage('two') {
+            sh "echo world"
+        }
+         stage('tree') {
+            sh "echo world"
+        }
+         stage('four') {
+            sh "echo world"
+        }
+
     } catch (err) {
         echo "Caught: ${err}"
         currentBuild.result = 'FAILURE'
