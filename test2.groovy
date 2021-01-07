e3e1b1b42b48149d36773577fff8c7cab6650921
arginbest <bsiitov84@gmail.com>
2021-01-07 00:09:27 +0000

    fromscript

--- test2.groovy
+++ test2.groovy
@@ -8,5 +8,16 @@ node{
             }
 
         }
-       failFast: true
+    //    failFast: true
+    post {
+        failure {
+            echo "A job failed"
+            script {
+                mail bcc: '', 
+                body: "Check console output at '${env.BUILD_URL}' error output = ${err}", 
+                cc: '', from: '', replyTo: '', subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", 
+                to: 'baurzhansiit@gmail.com'
+            }
+        }
+    }
 }
