af7a34cb910035ae75283d3dce7804d02ecc0b13
arginbest <bsiitov84@gmail.com>
2021-01-07 03:33:13 +0000

    fromscript

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -14,7 +14,7 @@ class Pipeline {
 //    Note : use "script" to access objects from jenkins pipeline run (WorkflowScript passed from Jenkinsfile)
 //           for example: script.node(), script.stage() etc
 script.pipeline {
-    script.stage('WorkflowScript') { 
+    script.dump('WorkflowScript') { 
         steps {
             sh "echo hello"
         }
